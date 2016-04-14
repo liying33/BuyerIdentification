b58c5356375e02f9054c3573da4be6c7e23a22a1
Lynn <lynn.zheng@sap.com>
2016-04-14 19:46:52 +0800

    Description:fisrt version main page JIRA: AWB-33333

---
+++ WebContent/hackathon/hackathon.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("hackathon.hackathon", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf hackathon.hackathon
+	*/ 
+	getControllerName : function() {
+		return "hackathon.hackathon";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf hackathon.hackathon
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
